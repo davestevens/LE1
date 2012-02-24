aeb24ce715c43e8c54a148f8db15dec01dda0a50
Dave Stevens <d.stevens@lboro.ac.uk>
2012-02-24 10:50:13 +0000

    added space to pthread argument

--- Assembler/generate.pl
+++ Assembler/generate.pl
@@ -76,7 +76,7 @@ foreach $arg (@ARGV) {
 	$dram_base_offset = $1;
     }
     elsif($arg =~ /-pthread/) {
-	$arguments .= '-I' . $le1_folder . '/' . $assembler_folder . '/includes/libraries/pthread/';
+	$arguments .= '-I' . $le1_folder . '/' . $assembler_folder . '/includes/libraries/pthread/ ';
     }
     elsif($arg =~ /-D(.+)=(.+)/) {
 	$arguments .= "-D$1=$2 ";
