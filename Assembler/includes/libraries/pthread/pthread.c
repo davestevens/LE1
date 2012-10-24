36f6e38cfb08deccc452b46f1cb5c6461f639ff2
Dave Stevens <d.stevens@lboro.ac.uk>
2012-10-24 11:17:54 +0100

    Added pthread_mutex_* operations.

--- Assembler/includes/libraries/pthread/pthread.c
+++ Assembler/includes/libraries/pthread/pthread.c
@@ -27,3 +27,33 @@ int vthread_join(pthread_t thread, void **status)
   }
   return ret.vthread_return;
 }
+
+/* vthread_mutex_init
+ */
+int vthread_mutex_init(pthread_mutex_t *mutex, pthread_mutexattr_t *attr)
+{
+  __vexasm2 ret;
+  ret = _asm2(0x3, mutex);
+  return ret.vthread_return;
+}
+
+int vthread_mutex_destroy(pthread_mutex_t *mutex)
+{
+  __vexasm2 ret;
+  ret = _asm2(0x4, mutex);
+  return ret.vthread_return;
+}
+
+int vthread_mutex_lock(pthread_mutex_t *mutex)
+{
+  __vexasm2 ret;
+  ret = _asm2(0x5, mutex);
+  return ret.vthread_return;
+}
+
+int vthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+  __vexasm2 ret;
+  ret = _asm2(0x6, mutex);
+  return ret.vthread_return;
+}
--- Assembler/includes/libraries/pthread/pthread.h
+++ Assembler/includes/libraries/pthread/pthread.h
@@ -17,3 +17,18 @@ int vthread_join(pthread_t, void **);
 
 #define pthread_create(thread, atts,func, args) vthread_create(thread, atts, func, args)
 #define pthread_join(thread, status) vthread_join(thread, status)
+
+/* Mutex */
+
+typedef unsigned int pthread_mutex_t;
+typedef unsigned int pthread_mutexattr_t;
+
+int vthread_mutex_init(pthread_mutex_t *, pthread_mutexattr_t *);
+int vthread_mutex_destroy(pthread_mutex_t *);
+int vthread_mutex_lock(pthread_mutex_t *);
+int vthread_mutex_unlock(pthread_mutex_t *);
+
+#define pthread_mutex_init(mutex, attr) vthread_mutex_init(mutex, attr)
+#define pthread_mutex_destroy(mutex) vthread_mutex_destroy(mutex)
+#define pthread_mutex_lock(mutex) vthread_mutex_lock(mutex)
+#define pthread_mutex_unlock(mutex) vthread_mutex_unlock(mutex)
